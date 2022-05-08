6241a604d90a82362ee123b2c990d767387ab383
Dongpk10 <vandong.tran@ynov.com>
2022-05-09 00:23:47 +0200

    002

--- app/cypress/integration/lightbox.spec.js
+++ app/cypress/integration/lightbox.spec.js
@@ -1,23 +1,84 @@
 /// <reference types="cypress" />
+
 describe('lightbox', () => {
+
     beforeEach(() => {
-        cy.visit('../../lightbox.html');
+        cy.visit('../../lightbox.html')
+    })
+
+    // 1
+    it('should open lightbox, click on image', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.dataCy('lightbox').should('be.visible');
     });
-    it('p should open lightbox,click on image', () => {
-        //cy.get('input[name=cypher-key]').clear().type(3);
-        //cy.get('textarea').type("Hello Word");
-        cy.get('[data-cy=cypher-openimage]').click();
-        //cy.get("#result").should('have.text','D');
+    // 2
+    it('should close light box by  clicking outside of lightbox', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.get("body").click(0,0);
+        cy.dataCy('lightbox').should('not.be.visible');
+    });
+    // 3
+    it('should add a like and update counters', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.dataCy('lightbox').scrollTo('bottom');
+        cy.dataCy('like').click();
+        //chech svg visible
 
-        //cy.get('[data-cy=cypher-closeimage1]').click();
-        cy.get('[data-cy=cypher-like]').click();
-        cy.get('[data-cy=cypher-unlike]').click();
-        cy.get('[data-cy= cypher-comment]').type("Awesome");
-        cy.get('[data-cy= cypher-publish]').click();
-        cy.get('[data-cy= cypher-comment]').type("");
-        cy.get('[data-cy= cypher-publish]').click();
+        cy.dataCy('unlike').should('be.visible');
+        cy.dataCy('like').should('not.be.visible');
+         //check counter-lightbox
+        cy.dataCy('like-counter').should('have.text',1);
 
+        //check overlay-counter
+        cy.get("body").click(0,0);
+        cy.dataCy('lightbox-overlay').trigger('mouseover');
+        cy.dataCy('lightbox-overlay-like_count').should('have.text',1);
+
+    });
+    // 4
+    it('should add, then delete a like and update counters', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.dataCy('lightbox').scrollTo('bottom');
+        cy.dataCy('like').click();
+        cy.dataCy('unlike').click();
+
+        //chech svg visible
+
+        cy.dataCy('unlike').should('not.be.visible');
+        cy.dataCy('like').should('be.visible');
+        //check counter-lightbox
+        cy.dataCy('like-counter').should('have.text',0);
+
+        //check overlay-counter
+        cy.get("body").click(0,0);
+        cy.dataCy('lightbox-overlay').trigger('mouseover');
+        cy.dataCy('lightbox-overlay-like_count').should('have.text',0);
 
     });
 
-});
+    // 5
+    it('should add a comment', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.dataCy('lightbox').scrollTo('bottom');
+        cy.dataCy('add-comment').type("Awsesome");
+        cy.dataCy('publish').click();
+
+        //check presence of comment
+        cy.dataCy('comment').should('have.text',"Awsesome");
+        cy.dataCy("author").should('have.text',"johndoe");
+        //check comment counter lightbox
+        cy.dataCy('comment-counter').should('have.text',"Hide 1 comment");
+        //counter overlay-lightbox
+        cy.get("body").click(0,0);
+        cy.dataCy('lightbox-overlay').trigger('mouseover');
+        cy.dataCy('comment-count_overlay').should('have.text',1);
+
+    });
+    // 6
+    it('should not add an empty comment, publish button disabled', () => {
+        cy.dataCy('lightbox-overlay').click();
+        cy.dataCy('lightbox').scrollTo('bottom');
+        cy.dataCy("publish").should("be.disabled");
+    });
+
+})
